bf12d29b89684406863e00858e0483ca3e078123
Pomidorkin <pbriuzgin@gmail.com>
2021-05-13 19:04:26 +0900

    New scenes & UI added

---
+++ Laser Defender/Assets/Spripts/Level.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    // Scene loding code
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
